24ea4f0cf5a63722d6147141e864df8860af87d5
najahaja <najahaja00@gmail.com>
2024-09-03 13:02:05 +0500

    packages added

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 
   plugins: [react()],
-  base:"/YoungDevInterns_ReactJs_Portfolio/", 
+  base:"/YoungDevInterns-ReactJS-Portfolio/", 
 });
