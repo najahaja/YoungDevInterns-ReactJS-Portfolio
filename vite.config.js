1ca3f71ef60865eac6168bbd37235c8651776640
najahaja <najahaja00@gmail.com>
2024-09-03 12:04:53 +0500

    Base changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-base:"/YoungDevInterns_ReactJs_Portfolio/",  
+
   plugins: [react()],
+  base:"/YoungDevInterns_ReactJs_Portfolio/", 
 });
