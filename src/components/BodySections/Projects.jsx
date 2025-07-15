7efe753ad1ba2c7a5a9635d7a81caaac46fef1b0
najahaja <najahaja00@gmail.com>
2025-07-15 21:37:18 +0500

    Updated Projects component with 4 videos and fixed rendering issues

--- src/components/BodySections/Projects.jsx
+++ src/components/BodySections/Projects.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faCode } from "@fortawesome/free-solid-svg-icons";
-import video1 from "../../../src/assets/Images/Connect-four.mp4"; 
-import video2 from "../../../src/assets/Images/Food Cart.mp4"; 
+import video1 from "../../../src/assets/Images/Connect-four.mp4";
+import video2 from "../../../src/assets/Images/Food Cart.mp4";
+import video3 from "../../../src/assets/Images/Inference-and-Evaluation-of-MedSAM-2-on-Brain-Tumor-Segmentation-BRATS-2019.mp4"
+import video4 from "../../../src/assets/Images/TaskB1.mp4";
 
 const Projects = () => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
@@ -15,9 +17,40 @@ const Projects = () => {
     setHoveredIndex(null);
   };
 
-  const videos = [
-    { src: video1, title: "Connect Four Game Using C++ with SFML Graphics", demo: "https://github.com/najahaja/Connect-four", source: "https://github.com/najahaja/Connect-four", lang: "C++", tools: "DevC++, GCC, Git, SFML" },
-    { src: video2, title: "CartApp with React and Vite", demo: "https://main--addcartap.netlify.app/", source: "https://github.com/najahaja/addcart", lang: "JavaScript, CSS, HTML", tools: "React, Vite, Webpack, Redux, React Router, Axios/Fetch" }
+  const projects = [
+    { 
+      src: video1, 
+      title: "Connect Four Game Using C++ with SFML Graphics", 
+      demo: "https://github.com/najahaja/Connect-four", 
+      source: "https://github.com/najahaja/Connect-four", 
+      lang: "C++", 
+      tools: "DevC++, GCC, Git, SFML" 
+    },
+    { 
+      src: video2, 
+      title: "CartApp with React and Vite", 
+      demo: "https://main--addcartap.netlify.app/", 
+      source: "https://github.com/najahaja/addcart", 
+      lang: "JavaScript, CSS, HTML", 
+      tools: "React, Vite, Webpack, Redux, React Router, Axios/Fetch" 
+    },
+   
+    { 
+      src: video3, 
+      title: "Inference-and-Evaluation-of-MedSAM-2-on-Brain-Tumor-Segmentation-BRATS-2019", 
+      demo: "https://github.com/najahaja/Inference-and-Evaluation-of-MedSAM-2-on-Brain-Tu-mor-Segmentation-BRATS-2019-", 
+      source: "https://github.com/najahaja/Inference-and-Evaluation-of-MedSAM-2-on-Brain-Tu-mor-Segmentation-BRATS-2019-", 
+      lang: "Python", 
+      tools: "Jupyter Notebook, Pytorch, Numpy, matplotlib"
+    },
+    { 
+      src: video4, 
+      title: "TumorDetection", 
+      demo: "https://github.com/najahaja/TumorDetection", 
+      source: "https://github.com/najahaja/TumorDetection", 
+      lang: "Python", 
+      tools: "Jupyter Notebook, Pytorch, Numpy, matplotlib,Streamlit"
+    }
   ];
 
   return (
@@ -25,57 +58,55 @@ const Projects = () => {
       id="projects"
       className="AboutBody grid grid-cols-1 md:grid-cols-2 gap-8 py-12 px-4 md:px-20 min-h-screen w-full relative bg-cover bg-no-repeat bg-fixed text-black"
     >
-      {videos.map((video, index) => (
+      {projects.map((project, index) => (
         <div
           key={index}
-          className="relative rounded text-xl text-white cursor-pointer overflow-hidden aspect-w-16 aspect-h-9 md:aspect-w-16 md:aspect-h-9"
+          className="relative rounded-lg text-xl text-white cursor-pointer overflow-hidden aspect-video w-full" // Ensures 16:9 ratio
           onMouseEnter={() => handleMouseEnter(index)}
           onMouseLeave={handleMouseLeave}
         >
-          <div className="card relative w-full h-full">
-            <video autoPlay muted loop className="w-full h-full object-cover">
-              <source src={video.src} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-            {hoveredIndex === index && (
-              <div className="card-overlay absolute top-0 left-0 w-full h-full bg-black/50 backdrop-blur-sm flex flex-col items-center justify-center gap-5 p-4">
-                <div className="font-bold text-center md:text-lg text-base">{video.title}</div>
-                <div className="links flex flex-col md:flex-row items-center justify-center gap-5">
-                  <a
-                    className="card-button md:text-lg text-base md:p-3 p-1 rounded-lg bg-green-600 font-semibold flex gap-2 items-center hover:bg-amber-400 hover:text-black"
-                    href={video.demo}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FontAwesomeIcon icon={faEye} />
-                    Live Demo
-                  </a>
-                  <a
-                    className="card-button md:text-lg text-base md:p-3 p-1 rounded-lg bg-green-600 font-semibold flex gap-2 items-center hover:bg-amber-400 hover:text-black"
-                    href={video.source}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FontAwesomeIcon icon={faCode} />
-                    View Source
-                  </a>
+          {project.isPlaceholder ? (
+            <div className="w-full h-full bg-black/50 flex items-center justify-center font-semibold text-xl hover:bg-black/80 hover:text-amber-400">
+              Coming Soon
+            </div>
+          ) : (
+            <>
+              <video autoPlay muted loop className="w-full h-full object-cover">
+                <source src={project.src} type="video/mp4" />
+              </video>
+              {hoveredIndex === index && (
+                <div className="card-overlay absolute top-0 left-0 w-full h-full bg-black/50 backdrop-blur-sm flex flex-col items-center justify-center gap-5 p-4">
+                  <div className="font-bold text-center md:text-lg text-base">{project.title}</div>
+                  <div className="links flex flex-col md:flex-row items-center justify-center gap-5">
+                    <a
+                      className="card-button md:text-lg text-base md:p-3 p-1 rounded-lg bg-green-600 font-semibold flex gap-2 items-center hover:bg-amber-400 hover:text-black"
+                      href={project.demo}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FontAwesomeIcon icon={faEye} />
+                      Live Demo
+                    </a>
+                    <a
+                      className="card-button md:text-lg text-base md:p-3 p-1 rounded-lg bg-green-600 font-semibold flex gap-2 items-center hover:bg-amber-400 hover:text-black"
+                      href={project.source}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FontAwesomeIcon icon={faCode} />
+                      View Source
+                    </a>
+                  </div>
+                  <div className="text-xs flex flex-col gap-1 items-center mt-4">
+                    <span>Written in: {project.lang}</span>
+                    <span>Tools: {project.tools}</span>
+                  </div>
                 </div>
-                <div className="text-xs flex flex-col gap-1 items-center mt-4">
-                  <span>Written in: {video.lang}</span>
-                  <span>Tools: {video.tools}</span>
-                </div>
-              </div>
-            )}
-          </div>
+              )}
+            </>
+          )}
         </div>
       ))}
-      {/* Placeholders for "Coming Soon" sections */}
-      <div className="soon bg-black/50 rounded flex items-center justify-center font-semibold text-xl text-white hover:bg-black/80 hover:text-amber-400 cursor-pointer">
-        Coming Soon
-      </div>
-      <div className="soon bg-black/50 rounded flex items-center justify-center font-semibold text-xl text-white hover:bg-black/80 hover:text-amber-400 cursor-pointer">
-        Coming Soon
-      </div>
     </div>
   );
 };
